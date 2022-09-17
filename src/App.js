68193a96bffa61d5c9dd1123fd28cc8e561c3173
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-17 12:28:45 +0500

    All Done I

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import HomeComponent from "./modules/home";
 
+
 // making up a parent container and other core components for our application 
 const Container = styled.div`
   display: flex;
@@ -9,6 +10,11 @@ const Container = styled.div`
   align-items: center;
   margin: 30px 0px 10px;
   font-family: 'Montserrat', sans-serif;
+  border: 1px solid lightgray;
+  height: 100vh;
+  width: 450px;
+  padding-top: 25px;
+  border-radius: 11px;
 `;
 
 const Header = styled.span`
