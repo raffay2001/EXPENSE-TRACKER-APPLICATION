f2fd7012ada5128b04885a8edd9e5b69ad4f8423
raffay2001 <raffay.ahmed009@gmail.com>
2022-09-17 09:49:50 +0500

    Integrated Styled Components In the App

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components";
+import HomeComponent from "./modules/home";
+
+// making up a parent container and other core components for our application 
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin: 30px 0px 10px;
+  font-family: 'Montserrat', sans-serif;
+`;
+
+const Header = styled.span`
+  color: black;
+  font-size: 25px;
+  font-weight: bold; 
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Header>Expense Tracker</Header>
+      <HomeComponent />
+    </Container>
   );
 }
 
---
+++ src/modules/home/index.js
@@ -0,0 +1,10 @@
+const HomeComponent = (props) => {
+    return (
+        <div>
+            Home Component
+        </div>
+    );
+}
+
+
+export default HomeComponent;
